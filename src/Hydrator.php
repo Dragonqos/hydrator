c4a89c07f2ab67d915b96799eb48d85116f4e6f3
Dragon <vlad@ecomitize.com>
2016-10-25 18:49:56 +0000

    fixed dotted option while getting 0 value

--- src/Hydrator.php
+++ src/Hydrator.php
@@ -462,10 +462,11 @@ class Hydrator
         $last_key = array_pop($keys);
 
         // walk/build the array to the specified key
-        while ($arr_key = array_shift($keys)) {
+        foreach($keys as $arr_key) {
             if (!array_key_exists($arr_key, $array_ptr)) {
                 $array_ptr[$arr_key] = [];
             }
+
             $array_ptr = &$array_ptr[$arr_key];
         }
 
